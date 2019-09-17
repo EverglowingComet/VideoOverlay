0f875badc4adf77eba9e7509315ef7d026381b7d
EverglowingComet <everglowingComet@hotmail.com>
2019-09-17 15:18:08 -0700

    Initial Commit

---
+++ VideoOverlay/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VideoOverlay
+//
+//  Created by MacMaster on 9/17/19.
+//  Copyright © 2019 MacMaster. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
